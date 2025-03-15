525e8c3a9c31418664785536892c9082814ede53
leonardomeireles55 <leomeireles55@hotmail.com>
2025-03-15 08:05:45 -0300

    feat: add pagination and sorting parameters to URL generation for improved data retrieval

--- src/features/shared/utils/helpers/urlByNameAndDateNew.ts
+++ src/features/shared/utils/helpers/urlByNameAndDateNew.ts
@@ -11,7 +11,7 @@ const urlByNameAndDateNew = (props: UrlAnalyticsByNameAndDatePropsNew) => {
   const startDate = formatDateWithTime(date.startYear, date.startMonth, date.startDay)
   const endDate = formatEndDateWithTime(date.endYear, date.endMonth, date.endDay)
 
-  const url = `${process.env.NEXT_PUBLIC_API_BASE_URL}/${analyticsType}/${process.env.NEXT_PUBLIC_API_BASE_URL_RESULTS_GROUPED}${testFormatFix(name)}&startDate=${startDate}&endDate=${endDate}`
+  const url = `${process.env.NEXT_PUBLIC_API_BASE_URL}/${analyticsType}/${process.env.NEXT_PUBLIC_API_BASE_URL_RESULTS_GROUPED}${testFormatFix(name)}&startDate=${startDate}&endDate=${endDate}&pageSize=200&sort=date,asc`
 
   return { url }
 }
