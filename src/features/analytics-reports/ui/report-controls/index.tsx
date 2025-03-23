24b4e0fcc8fa1c7ae1b7c7e43ac1cd139738a62d
leonardomeireles55 <leomeireles55@hotmail.com>
2025-03-22 23:32:36 -0300

    refactor: update layout of report controls for improved responsiveness and clarity

--- src/features/analytics-reports/ui/report-controls/index.tsx
+++ src/features/analytics-reports/ui/report-controls/index.tsx
@@ -13,11 +13,10 @@ const ReportsControls = ({
 }: ReportsControlsProps) => (
   <div className='grid gap-2 text-xs text-textSecondary md:flex md:flex-col xl:mt-24'>
     <DateSelector {...dateSelectorProps} />
-    <div className='flex items-end gap-2'>
+    <div className='grid items-end gap-2 md:flex'>
       <span className='text-textPrimary'>AnalyticsType:</span>
       <AnalyticsTypeSelector analyticsType={analyticsType} onChange={onAnalyticsTypeChange} />
       <span className='text-textPrimary'>Action:</span>
-
       <GeneratePdf
         jsonData={dataFetched}
         fileName={analyticsType}
